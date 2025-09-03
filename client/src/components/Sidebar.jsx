c2f40787ea231afaa28bd07a19d75a527872d366
DarrenDong0426 <ddarren@umich.edu>
2025-09-02 23:18:38 -0400

    Basic framework frontend for sidebar and documents page

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Sidebar(){
+    return(
+        <div className="h-screen w-full border-r border-gray-500 p-4">
+            
+        </div>
+    )
+}
+
--- client/src/pages/Documents.jsx
+++ client/src/pages/Documents.jsx
@@ -1,13 +1,24 @@
 // Imports
 import Header from "../components/Header"
+import Sidebar from "../components/Sidebar"
 
 export default function Documents(){
     return (
-        <div>
+        <div className='min-h-screen flex flex-col bg-gray-50'>
             <Header/>
-            <div>
-                
-            </div>
+            <main className='flex-1 overflow-y-auto py-8'>
+                <h1 className='text-4xl font-bold mb-8 text-center'>Documents</h1>
+                <div className="flex border-t border-gray-500">
+                    <div className="flex flex-[1]">
+                        <Sidebar/>
+                    </div>
+                    <div className="flex flex-[4] justify-center">
+                        <div>
+                            Title
+                        </div>
+                    </div>
+                </div>
+             </main>
         </div>
     )
 }
